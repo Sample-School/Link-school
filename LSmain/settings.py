1a78f4c8312b9cacd3e2f6742a36a1171a8752ab
JVAmaralR <joaoamaral051@gmail.com>
2025-04-09 15:08:28 -0300

    correção de bugs

--- LSmain/settings.py
+++ LSmain/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'LSmain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'base',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
