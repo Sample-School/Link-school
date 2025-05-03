c6322deeffd550abe206ab7c37a70c577ed77bb2
JVAmaralR <joaoamaral051@gmail.com>
2025-05-03 13:45:22 -0300

    correção de bugs e caminho do login do cliente

--- LSCliente/urls.py
+++ LSCliente/urls.py
@@ -16,13 +16,13 @@ urlpatterns = [
 
     #urls para recuperação de conta
 path('clientepassword_reset/', auth_views.PasswordResetView.as_view(
-    template_name='password_reset/password_reset_email_form.html',
-    email_template_name='password_reset/email.html',
-    html_email_template_name='password_reset/email.html',
+    template_name='cliente_password_reset/password_reset_email_form.html',
+    email_template_name='cliente_password_reset/email.html',
+    html_email_template_name='cliente_password_reset/email.html',
     form_class=CustomPasswordResetForm  
 ), name='clientepassword_reset'),
-path('clientepassword_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset/password_reset_email_enviado.html'), name='clientepassword_reset_done'), #Rota que confirma o envio do email
-path('clientepassword_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset/password_reset_senha_nova_form.html', form_class=NewResetPasswordForm), name='clientepassword_reset_confirm'), #Rota para a tela de redefinir senha
-path('clientepassword_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_senha_trocada.html'), name='clientepassword_reset_complete'), #Rota para a confirmação de senha trocada
+path('clientepassword_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='cliente_password_reset/password_reset_email_enviado.html'), name='clientepassword_reset_done'), #Rota que confirma o envio do email
+path('clientepassword_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='cliente_password_reset/password_reset_senha_nova_form.html', form_class=NewResetPasswordForm), name='clientepassword_reset_confirm'), #Rota para a tela de redefinir senha
+path('cliente_password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_senha_trocada.html'), name='clientepassword_reset_complete'), #Rota para a confirmação de senha trocada
 
 ]
